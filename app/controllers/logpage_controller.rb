2345e3eab811f9bf745c1796efbe4d0834154142
vxvinod <vinodkmrmurugan@gmail.com>
2015-06-04 19:12:26 +0530

    xml alignment done

--- app/controllers/logpage_controller.rb
+++ app/controllers/logpage_controller.rb
@@ -1,5 +1,5 @@
 class LogpageController < ApplicationController
-
+include LogpageHelper
 	def searchLog
 		@log = PathLocation.new
 		#@display = File.readlines('C:\Users\60010743\Desktop\proxy.log').each{|line| line}
@@ -8,7 +8,8 @@ class LogpageController < ApplicationController
 	def create
 		@log = PathLocation.new
 		@path = params[:path_location][:path]
-		@display = File.readlines(@path).each{|line| line}
+		#@display = File.readlines(@path).each{|line| line}
+		@display = format_full_log(@path)#.gsub(/\n/, '<br />')
 		render template: "logpage/searchLog.html"
 	end
 end
--- app/helpers/logpage_helper.rb
+++ app/helpers/logpage_helper.rb
@@ -1,2 +1,18 @@
+require "rexml/document"
+require 'debugger'
 module LogpageHelper
+	def format_full_log path
+		#debugger
+		whole_out = ""
+		lines = File.readlines(@path).each{|line| line}
+		lines.join('').split("Response").each do |data|
+			#debugger
+			doc = REXML::Document.new data
+			out=""
+			doc.write(out,1)
+			whole_out << out
+		end
+
+		whole_out
+	end
 end
